d5cd55d1bbdd83ca3370311436e3e08e862a76cf
Pedro Ligabue <pedroligabue@gmail.com>
2021-10-11 01:35:48 -0300

    v0.5

--- build_kg_nodes.py
+++ build_kg_nodes.py
@@ -1,28 +1,37 @@
+from re import sub
 import pandas as pd
 from pathlib import Path
 import argparse
+import json
+import re
 
 from tqdm import tqdm
 
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
-from official.nlp import optimization  # to create AdamW optimizer
 
-import matplotlib.pyplot as plt
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--threshold", type=float, default=-0.95)
+parser.add_argument("--small", dest="size", action="store_const", const="small")
+parser.add_argument("--medium", dest="size", action="store_const", const="medium")
+args = parser.parse_args()
+
+THRESHOLD = args.threshold
+SIZE = args.size or "small"
 
 tfhub_handle_preprocess = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
-tfhub_handle_encoder = "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1"
+
+if SIZE == "medium":
+    tfhub_handle_encoder = "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-512_A-8/2" # SMALL BERT
+else:
+    tfhub_handle_encoder = "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/2" # MEDIUM BERT
 
 bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
 bert_model = hub.KerasLayer(tfhub_handle_encoder)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--threshold", type=float, default=-0.95)
-args = parser.parse_args()
-THRESHOLD = args.threshold
 
-def get_similarity_matrix(df, embeddings):
+def get_similarity_matrix(embeddings):
 
     cosine_loss = tf.keras.losses.CosineSimilarity(axis=1)
     
@@ -38,100 +47,70 @@ def get_similarity_matrix(df, embeddings):
     return similarity_matrix
 
 
-def get_above_threshold_results(similarity_matrix):
-
-    above_threshold_results = []
-    for i, row in enumerate(similarity_matrix):
-        indexes = []
-        similarities = []
-        for j, value in enumerate(row):
-            if i == j:
-                continue
-            if value < THRESHOLD:
-                indexes.append(j)
-                similarities.append(value)
-        
-        above_threshold_results.append({
-            "indexes": indexes,
-            "similarities": similarities,
-            "avg_similarity": sum(similarities)/len(similarities) if similarities else 0.0
-        })
-
-    return above_threshold_results
-
-def group_triples(above_threshold_results):
-
-    group_indexes = [None for _ in above_threshold_results]
-    current_group_index = -1
-    
-    for i, above_threshold_result in enumerate(above_threshold_results):
-        if group_indexes[i] is None:
-            current_group_index += 1
-
-            group_indexes[i] = current_group_index
-            for similar_index in above_threshold_result["indexes"]:
-                group_indexes[similar_index] = current_group_index
-        else:
-            group_index = group_indexes[i]
-            for similar_index in above_threshold_result["indexes"]:
-                group_indexes[similar_index] = group_index
-
-    groups = []
-    for group_index in set(group_indexes):
-        group = []
-        for i, _ in enumerate(above_threshold_results):
-            if group_indexes[i] == group_index:
-                group.append(i)
-        groups.append(group)
-
-    return groups   
-
-def get_node_from_group(df, above_threshold_results, group):
-    group_results = [{"index": i, "result": result} for i, result in enumerate(above_threshold_results) if i in group]
-    group_results.sort(key=lambda res: res["result"]["avg_similarity"])
-    
-    most_similar_index = group_results[0]["index"]
-    for result in group_results:
-        subject = df.iloc[result["index"]]["subject"].lower()
-        if subject not in ["i", "you", "he", "she", "it", "they"]:
-            most_similar_index = result["index"]
-            break
-
-    return df.iloc[most_similar_index]
-
 def main():
 
     TRIPLE_DIR = Path("./triples")
-    KG_NODE_DIR = Path(f"./kg_nodes_{int(-100 * THRESHOLD)}")
+    KG_NODE_DIR = Path(f"./kg_nodes_{int(-100 * THRESHOLD)}_{SIZE}")
     KG_NODE_DIR.mkdir(exist_ok=True)
 
     failed_files = []
     empty_files = []
     for file in tqdm(list(TRIPLE_DIR.glob("*.txt"))):
         try:
-            with open(file, encoding="utf-8") as f:
-                base_df = pd.read_csv(file, sep=";", names=["confidence", "subject", "relation", "object"])
-                df = base_df[base_df["confidence"] > 0.9]
-                if df.empty:
-                    empty_files.append(file.name)
-                    continue
-                sentences = df.apply(lambda row: " ".join(row[["subject", "relation", "object"]]).replace("�", ""), axis=1)
-                text_preprocessed = bert_preprocess_model(sentences)
-                bert_results = bert_model(text_preprocessed)["default"]
-                sim_matrix = get_similarity_matrix(df, bert_results)
-                above_threshold_results = get_above_threshold_results(sim_matrix)
-                groups = group_triples(above_threshold_results)
-                
-            with open(KG_NODE_DIR / file.name, "w", encoding="utf-8") as f:
-                nodes = []
-                for group in groups:
-                    # node = get_node_from_group(df, above_threshold_results, group)
-                    subjects = "|".join(set(df.iloc[group]["subject"]))
-                    relations = "|".join(set(df.iloc[group]["relation"]))
-                    objects = "|".join(set(df.iloc[group]["object"]))
-                    node_string = f'{subjects};{relations};{objects}'
-                    nodes.append(node_string)
-                f.write("\n".join(nodes))
+            base_df = pd.read_csv(file, sep=";", names=["confidence", "subject", "relation", "object"])
+            df = base_df[base_df["confidence"] > 0.9]
+            if df.empty:
+                empty_files.append(file.name)
+                continue
+
+            triples = df.apply(lambda row: " ".join(row[["subject", "relation", "object"]]).replace("�", ""), axis=1)
+            triples_preprocessed = bert_preprocess_model(triples)
+            triples_encodings = bert_model(triples_preprocessed)["pooled_output"]
+
+            subjects = df["subject"]
+            subjects_preprocessed = bert_preprocess_model(subjects)
+            base_subjects_encodings = bert_model(subjects_preprocessed)["pooled_output"]
+            subjects_encodings = base_subjects_encodings + triples_encodings
+            subjects_matrix = get_similarity_matrix(subjects_encodings)
+
+            objects = df["subject"]
+            objects_preprocessed = bert_preprocess_model(objects)
+            base_objects_encodings = bert_model(objects_preprocessed)["pooled_output"]
+            objects_encodings = base_objects_encodings + triples_encodings
+            objects_matrix = get_similarity_matrix(objects_encodings)
+
+            if len(subjects_matrix) != len(objects_matrix):
+                raise Exception("Matrices have different lengths")
+
+            nodes = []
+            for i, _ in enumerate(subjects_matrix):
+                subject_results = subjects_matrix[i]
+                subject_node_indexes = [j for j, v in enumerate(subject_results) if v < THRESHOLD]
+                subject_node = list(set(df.iloc[subject_node_indexes]["subject"]))
+
+                relation = df.iloc[i]["relation"]
+
+                object_results = objects_matrix[i]
+                object_node_indexes = [j for j, v in enumerate(object_results) if v < THRESHOLD]
+                object_node = list(set(df.iloc[object_node_indexes]["object"]))
+
+                try:
+                    same_node_index = next(j for j, node in enumerate(nodes) if node["subject"] == subject_node and node["relation"] == relation)
+                    nodes[same_node_index]["object"] += object_node
+                except StopIteration:
+                    nodes.append({
+                        "subject": subject_node,
+                        "relation": relation,
+                        "object": object_node
+                    })
+
+            with open(KG_NODE_DIR / f"{file.stem}.json", "w", encoding="utf-8") as f:
+                json_string = json.dumps(nodes, indent=2)
+                json_string = re.sub('\n {6}|\n {4}(?=\])', " ", json_string)
+                json_string = re.sub('\[ ', "[", json_string)
+                json_string = re.sub(' \]', "]", json_string)
+                f.write(json_string)
+
         except Exception as e:
             failed_files.append(f"{file.name} - {e}")
 
--- merge_nodes.py
+++ merge_nodes.py
@@ -1,22 +1,18 @@
 from pathlib import Path
+import json
 
 def main():
     CURRENT_DIR = Path()
     dirs = [dir for dir in CURRENT_DIR.glob("kg_nodes*") if dir.is_dir()]
 
     for dir in dirs:
-        nodes = []
-        for file in dir.glob("*.txt"):
+        nodes = {}
+        for file in dir.glob("*.json"):
             with open(file) as f:
-                for line in f:
-                    node = line.strip()
-                    nodes.append(node)
+                nodes[file.stem] = json.load(f)
 
-        nodes.sort(key=str.casefold)
-        merged_node_text = "\n".join(nodes)
-
-        with open(f"results/{dir.name}.summary.txt", "w", encoding="utf-8") as f:
-            f.write(merged_node_text)
+        with open(f"results/{dir.name}.summary.json", "w", encoding="utf-8") as f:
+            json.dump(nodes, f, indent=2)
 
 if __name__ == "__main__":
     main()
