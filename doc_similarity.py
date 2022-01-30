21c8b8c65b1bf71ba5e61d4e6669ab9e79b783b0
Pedro Ligabue <pedroligabue@gmail.com>
2022-01-30 20:18:57 -0300

    v4.0.0-beta.4
    Import KG graph builder instead of redefining functions.

--- doc_similarity.py
+++ doc_similarity.py
@@ -3,38 +3,21 @@ import argparse
 import json
 
 import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text as text
 
+from build_kg_nodes import get_models, get_similarity_matrix
 from cli_args import SIZE, MATCH
-
-tfhub_handle_preprocess = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
-tfhub_handle_encoder = {
-    "big": "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4",
-    "medium": "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-512_A-8/2",
-    "small": "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/2"
-}[SIZE]
-
-text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
-preprocessor = hub.KerasLayer(tfhub_handle_preprocess)
-encoder_inputs = preprocessor(text_input)
-encoder = hub.KerasLayer(tfhub_handle_encoder)
-outputs = encoder(encoder_inputs)
-cls_model = tf.keras.Model(text_input, outputs["pooled_output"])
 
 def read_file(file):
     with open(file, "r", encoding="utf-8") as f:
         return f.read()
-
-def get_similarity_matrix(embeddings):
-    normalized_embeddings = tf.math.l2_normalize(embeddings, 1)
-    return tf.linalg.matmul(normalized_embeddings, normalized_embeddings, transpose_b=True)
 
 def main():
     SENTENCE_DIR = Path('./sentences')
     RESULTS_DIR = Path('./results')
 
     KG_NODE_DIRS = [dir for dir in RESULTS_DIR.glob(MATCH) if dir.is_dir()]
+    
+    _, _, cls_model = get_models(size=SIZE)
     
     for dir in KG_NODE_DIRS:
         base_dir = dir / "base"
--- group_graphs.py
+++ group_graphs.py
@@ -8,34 +8,13 @@ from datetime import datetime
 
 from tqdm import tqdm
 
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text as text
-
+from build_kg_nodes import get_models, build_graph
 from cli_args import GROUP_SIZE, MATCH, SIZE, RATIO, THRESHOLD
 
-
-tfhub_handle_preprocess = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
-tfhub_handle_encoder = {
-    "big": "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4",
-    "medium": "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-512_A-8/2",
-    "small": "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/2"
-}[SIZE]
-
-text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
-preprocessor = hub.KerasLayer(tfhub_handle_preprocess)
-encoder_inputs = preprocessor(text_input)
-encoder = hub.KerasLayer(tfhub_handle_encoder)
-outputs = encoder(encoder_inputs)
-cls_model = tf.keras.Model(text_input, outputs["pooled_output"])
 
 def read_json(file):
     with open(file, "r", encoding="utf-8") as f:
         return json.load(f)
-
-def get_similarity_matrix(embeddings):
-    normalized_embeddings = tf.math.l2_normalize(embeddings, 1)
-    return tf.linalg.matmul(normalized_embeddings, normalized_embeddings, transpose_b=True)
 
 def get_sorted_mappings(filenames, sim_matrix):
     mappings = []
