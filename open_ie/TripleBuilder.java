1314a62529845919a920f42d32bf934a19df76a1
Pedro Ligabue <pedroligabue@gmail.com>
2022-03-09 18:14:21 -0300

    v4.3.3
    Slight change to the OpenIE pipeline.

--- open_ie/TripleBuilder.java
+++ open_ie/TripleBuilder.java
@@ -26,9 +26,9 @@ public class TripleBuilder {
         props.setProperty("openie.ignore_affinity", "true"); // WHEN TRUE, REMOVES TRIPLES THAT HAVE AFFINITY BELOW 1.0. MIGHT BE USEFUL COMBINED WITH THE affinity_probability_cap PROPERTY.
         props.setProperty("openie.affinity_probability_cap", "1.0"); // SETTING IT TO 1.0 LEAVES ALL AFFINITY VALUES UNROUNDED. MIGHT BE USEFUL COMBINED WITH THE ignore_affinity PROPERTY.
         props.setProperty("openie.triple.strict", "false"); // WHEN FALSE, THE TRIPLES CHANGE SLIGHTLY. IT SEEMS MARGINALLY BETTER.
-        // props.setProperty("openie.triple.all_nominals", "true"); // WHEN TRUE, THE NUMBER OF TRIPLES INCREASE A SMALL AMOUNT. THE EXTRA TRIPLES DO NOT SEEM VERY USEFUL THOUGH, THEY ARE MOSTLY INDICATING ADJECTIVES.
+        // props.setProperty("openie.triple.all_nominals", "true"); // WHEN TRUE, THE NUMBER OF TRIPLES INCREASES A SMALL AMOUNT. THE EXTRA TRIPLES DO NOT SEEM VERY USEFUL THOUGH, THEY ARE MOSTLY INDICATING ADJECTIVES.
         // props.setProperty("openie.splitter.model", ""); // WILL NOT BE USED.
-        props.setProperty("openie.splitter.nomodel", "true"); // DOES NOT CHANGE MUCH. SLIGHTLY BETTER AS TRUE.
+        // props.setProperty("openie.splitter.nomodel", "true"); // DOES NOT CHANGE MUCH.
         // props.setProperty("openie.splitter.disable", "true"); // THIS IS AN IMPORTANT ONE. WHEN TRUE, THE NUMBER OF TRIPLES IS MINIMAL.
         // props.setProperty("openie.affinity_models", ""); // WILL NOT BE USED.
         props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,depparse,coref,natlog,openie");
