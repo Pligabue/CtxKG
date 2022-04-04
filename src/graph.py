a6c20f876cd44a89fa1bef9a9e439c1b4ed22629
Pedro Ligabue <pedroligabue@gmail.com>
2022-04-04 12:07:23 -0300

    v7.0.0-alpha.4
    Added a way to load the graph from the generated JSON file.
    
    Added basic cleaning. Still missing a way to remove duplicate triples
    and triples where the subject and object are the same.

--- src/graph.py
+++ src/graph.py
@@ -32,7 +32,26 @@ class Graph:
             subject = graph.get_entity_by_id(subject_id, subject_text)
             object = graph.get_entity_by_id(object_id, object_text)
             graph.add_triple(subject, relation, object, confidence=confidence) 
+        return graph
 
+    @staticmethod
+    def from_json(filepath):
+        graph = Graph([filepath])
+        with open(filepath) as f:
+            graph_json = json.load(f)
+            for id, text in graph_json["entities"].items():
+                graph.add_entity(id, text)
+            for node in graph_json["graph"]:
+                subject = graph.get_entity_by_id(node["subject_id"])
+                relation = node["relation"]
+                object = graph.get_entity_by_id(node["object_id"])
+                graph.add_triple(subject, relation, object)
+            for id, links in graph_json["links"].items():
+                entity = graph.get_entity_by_id(id)
+                for link in links:
+                    linked_entity = graph.get_entity_by_id(link)
+                    if not graph.link_exists(entity, linked_entity):
+                        graph.add_link(entity, linked_entity)
         return graph
 
     def get_entity_by_id(self, entity_id, entity_text=""):
@@ -59,6 +78,12 @@ class Graph:
         self.links.append(new_link)
         return new_link
 
+    def get_linked_entities(self, entity):
+        return [l.entity_a if l.entity_b is entity else l.entity_b for l in self.links if l.entity_a is entity or l.entity_b is entity]
+
+    def link_exists(self, entity_a, entity_b):
+        return entity_b in self.get_linked_entities(entity_a)
+
     def build_entity_encodings(self):
         subject_encodings, object_encodings = self.encoder.get_entity_encodings(self.triples)
         for triple, subject_encoding, object_encoding in zip(self.triples, subject_encodings, object_encodings):
@@ -78,14 +103,11 @@ class Graph:
                     self.add_link(entity_a, entity_b)
         return self
 
-    def get_linked_entities(self, entity):
-        return [l.entity_a if l.entity_b is entity else l.entity_b for l in self.links if l.entity_a is entity or l.entity_b is entity]
-
     def save_json(self, filepath):
         graph_json = {
             "documents": self.filepaths,
             "entities": {entity_id: entity.text for entity_id, entity in self.entities.items()},
-            "graph": [{"subject_id": triple.subject.id, "relation": triple.relation, "object": triple.object.id} for triple in self.triples],
+            "graph": [{"subject_id": triple.subject.id, "relation": triple.relation, "object_id": triple.object.id} for triple in self.triples],
             "links": {entity_id: [linked_entity.id for linked_entity in self.get_linked_entities(entity)] for entity_id, entity in self.entities.items()}
         }
         with open(filepath, "w", encoding="utf-8") as f:
@@ -94,10 +116,31 @@ class Graph:
     def number_of_appearences(self, entity):
         return len([triple for triple in self.triples if triple.includes_entity(entity)])
 
+    def get_replacement(self, pool, sorted_by_appearences):
+        for entity in sorted_by_appearences:
+            if entity in pool:
+                return entity
+        return None
+
     def replace_entity(self, original, replacement):
         del self.entities[original.id]
         for triple in self.triples:
             triple.replace_entity(original, replacement)
 
     def clean(self):
-        pass
+        sorted_entities = sorted(self.entities.values(), key=lambda entity: self.number_of_appearences(entity), reverse=True)
+        for triple in self.triples:
+            subject_pool = [triple.subject] + self.get_linked_entities(triple.subject)
+            subject_replacement = self.get_replacement(subject_pool, sorted_entities)
+            if subject_replacement is not triple.subject and subject_replacement is not None:
+                self.replace_entity(triple.subject, subject_replacement)
+                sorted_entities.remove(triple.subject)
+
+            object_pool = [triple.object] + self.get_linked_entities(triple.object)
+            object_replacement = self.get_replacement(object_pool, sorted_entities)
+            if object_replacement is not triple.object and object_replacement is not None:
+                self.replace_entity(triple.object, object_replacement)
+                sorted_entities.remove(triple.object)
+        self.links = []
+
+        return self
