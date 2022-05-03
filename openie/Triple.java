065902f6a949d34cf6ba5eba2524721373c4a185
Pedro Ligabue <pedroligabue@gmail.com>
2022-05-03 16:32:17 -0300

    Create Triple class for OpenIE
    This class is responsible for keeping triple references. This includes
    keeping the subject Entity, the relation and the object Entity.
    
    It is also responsible for building the CSV row with its content, which
    includes the triples from the breakdown of both the subject and the
    object.

---
+++ openie/Triple.java
@@ -0,0 +1,164 @@
+package openie;
+
+
+import openie.Entity;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreEntityMention;
+import edu.stanford.nlp.ie.util.RelationTriple;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphEdge;
+import edu.stanford.nlp.ling.IndexedWord;
+
+
+public class Triple {
+    private static String defaultRelation = "compound";
+
+    private double confidence;
+    private Entity subject;
+    private Entity object;
+    private String relation;
+    private SemanticGraph graph;
+
+    public static String getHeader(String path) {
+        return "# " + path + "\n" + "confidence;subject;relation;object;subject_id;object_id\n";
+    }
+
+    public Triple(RelationTriple triple, Entity subject, Entity object) {
+        this.confidence = triple.confidence;
+        this.subject = subject;
+        this.object = object;
+        this.relation = triple.relationGloss();
+        this.graph = triple.asDependencyTree().orElse(null);
+    }
+
+    
+    public Triple(Entity subject, String relation, Entity object) {
+        this.confidence = 1.0;
+        this.subject = subject;
+        this.object = object;
+        this.relation = relation;
+        this.graph = null;
+    }
+
+    public boolean subjectAndObjectAreDifferent() {
+        return !this.subject.equals(this.object);
+    }
+
+    public List<Triple> buildAllTriples() {
+        List<Triple> allTriples = new ArrayList<>();
+        allTriples.add(this);
+        allTriples.addAll(this.subject.getDerivativeTriples());
+        allTriples.addAll(this.object.getDerivativeTriples());
+        return allTriples;
+    }
+
+    public boolean notEmpty() {
+        return !this.subject.getFinalEntity().cleanForm().getTokens().isEmpty() && !this.object.getFinalEntity().cleanForm().getTokens().isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        Entity finalSubject = this.subject.getFinalEntity().cleanForm();
+        Entity finalObject = this.object.getFinalEntity().cleanForm();
+        return this.confidence + ";" + finalSubject.getText() + ";" + this.relation + ";" + finalObject.getText() + ";" + finalSubject.getId() + ";" + finalObject.getId();
+    }
+
+    public static Triple buildTriple(Entity subject, Entity object, SemanticGraph graph) {
+        String relation = findRelation(subject, object, graph);
+        switch (relation) {
+            case "advmod":
+            case "advmod:emph":
+            case "advmod:lmod":
+            case "csubj":
+            case "csubj:pass":
+            case "goeswith":
+            case "nsubj":
+            case "nsubj:pass":
+            case "obj":
+            case "obl":
+            case "obl:agent":
+            case "obl:arg":
+            case "obl:lmod":
+            case "obl:tmod":
+            case "xcomp":
+                return new Triple(subject, "at", object);
+            case "amod":
+            case "appos":
+                return new Triple(subject, "is", object);
+            case "compound":
+            case "nmod":
+                return new Triple(subject, "of", object);
+            case "conj":
+                return new Triple(subject, "and", object);
+            case "nmod:poss":
+            case "nmod:tmod":
+                return new Triple(object, "has", subject);
+            case "nummod":
+            case "nummod:gov":
+                return new Triple(subject, "amount to", object);
+            case "vocative":
+                return new Triple(object, "referenced by", subject);
+            case "acl":
+            case "acl:relcl":
+            case "advcl":
+            case "aux":
+            case "aux:pass":
+            case "case":
+            case "cc":
+            case "cc:preconj":
+            case "ccomp":
+            case "clf":
+            case "compound:lvc":
+            case "compound:prt":
+            case "compound:redup":
+            case "compound:svc":
+            case "cop":
+            case "dep":
+            case "det":
+            case "det:numgov":
+            case "det:nummod":
+            case "det:poss":
+            case "discourse":
+            case "dislocated":
+            case "expl":
+            case "expl:impers":
+            case "expl:pass":
+            case "expl:pv":
+            case "fixed":
+            case "flat":
+            case "flat:foreign":
+            case "flat:name":
+            case "iobj":
+            case "list":
+            case "mark":
+            case "orphan":
+            case "parataxis":
+            case "punct":
+            case "reparandum":
+            case "root":
+            default:
+                return new Triple(subject, relation, object);
+        }
+    }
+
+    private static String findRelation(Entity subject, Entity object, SemanticGraph graph) {
+        String relation = defaultRelation;
+        List<CoreLabel> subjectTokens = subject.getTokens();
+        List<CoreLabel> objectTokens = object.getTokens();
+
+        for (SemanticGraphEdge edge : graph.edgeIterable()) {
+            CoreLabel source = edge.getSource().backingLabel();
+            CoreLabel target = edge.getTarget().backingLabel();
+            if (subjectTokens.contains(source) && objectTokens.contains(target)) {
+                relation = edge.getRelation().getShortName();
+                break;
+            }
+        }
+        return relation;
+    }
+}
