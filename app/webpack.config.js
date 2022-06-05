a6aea4342a859e306d9dda4b6fc0e3db1690919c
Pedro Ligabue <pedroligabue@gmail.com>
2022-06-05 13:17:38 -0300

    Move graph JS to top-level graph directory

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,7 +3,7 @@ const { VueLoaderPlugin } = require('vue-loader')
 module.exports = {
   mode: "production",
   entry: {
-    graph: { import: "./javascript/graph", filename: "./results/graphs/index.js" }
+    graph: { import: "./graph", filename: "./results/graphs/index.js" }
   },
   output: {
     path: __dirname + "/static"
