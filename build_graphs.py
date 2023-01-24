fa334e1ef8cca9ce6599aba0d0b7962fe34eca5e
Pedro Ligabue <pedroligabue@gmail.com>
2023-01-24 16:55:23 -0300

    Improve file and directory selection when building graphs

--- build_graphs.py
+++ build_graphs.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import datetime
 from tqdm import tqdm
 from tkinter import Tk
-from tkinter.filedialog import askopenfilenames
+from tkinter.filedialog import askopenfilenames, askdirectory
 
 from src.graph import Graph
 from src.encoder import Encoder
@@ -11,29 +11,50 @@ from constants import TRIPLE_DIR, RESULT_DIR
 from cli_args import SIZE, RATIO, THRESHOLD, OVERWRITE, MATCH, NAME, BATCH
 
 
-def get_files(base_dir, match, overwrite):
+def get_target_dir(base_dir):
+    window = Tk()
+    window.withdraw()
+    window.attributes("-topmost", 1)
+    directory = askdirectory(initialdir=base_dir)
+    return Path(directory)
+
+def get_files(match):
     if match is None:
         Tk().withdraw()
         filenames = askopenfilenames(initialdir=TRIPLE_DIR)
         filepaths = [Path(f) for f in filenames]
     else:
         filepaths = list(TRIPLE_DIR.glob(match))
-    return [f for f in filepaths if f.suffix == ".csv" and (overwrite or not (base_dir / f"{f.stem}.json").is_file())]
+    return [f for f in filepaths if f.suffix == ".csv" and f.is_file()]
+
+def save_params(dir: Path, size, ratio, threshold, overwrite, match, name, batch_size):
+    with (dir / "PARAMS.md").open("w", encoding="utf-8") as f:
+        params = (f"BERT SIZE: {size}\n" \
+                  f"RATIO (entity encoding over full sentence encoding): {ratio}\n" \
+                  f"THRESHOLD: {threshold}\n" \
+                  f"OVERWRITE: {overwrite}\n" \
+                  f"FILE NAME MATCHING: {match}\n" \
+                  f"GROUP NAME: {name}\n" \
+                  f"ENCODING BATCH SIZE: {batch_size}\n")
+        f.write(params)
 
 def main(size, ratio, threshold, overwrite, match, name, batch_size):
-    kg_dir = RESULT_DIR / (name or f"kg_nodes_ratio_{int(100 * ratio)}_threshold_{int(100 * threshold)}_{size}")
+    files = get_files(match)
+
+    kg_dir = RESULT_DIR / name if name else get_target_dir(RESULT_DIR)
     kg_dir.mkdir(exist_ok=True)
+    save_params(kg_dir, size, ratio, threshold, overwrite, match, name, batch_size)
+
     base_dir = kg_dir / "base"
     base_dir.mkdir(exist_ok=True)
     errors_dir = base_dir / "errors"
     errors_dir.mkdir(exist_ok=True)
-
-    failed_files = []
-    files = get_files(base_dir, match, overwrite)
-    sorted_files = sorted(files, key=lambda file: file.stat().st_size)
+    
+    filtered_files = [f for f in files if overwrite or not (base_dir / f"{f.stem}.json").is_file()]
+    sorted_files = sorted(filtered_files, key=lambda file: file.stat().st_size)
 
     encoder = Encoder(size=size, ratio=ratio)
-
+    failed_files = []
     for file in tqdm(sorted_files):
         try:
             graph = Graph.from_csv(file, encoder)
