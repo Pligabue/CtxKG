c4ae54e4c2068ddd5a06f66520979b1b075cf220
Pedro Ligabue <pedroligabue@gmail.com>
2023-10-16 18:51:15 -0300

    Add form to create a new batch
    The "new" action is not finished, as it is not triggering the graph
    generation. The form, however, should be fully functioning (field types,
    validations etc.), as should the logic for the two paths (valid form and
    new/invalid form).

--- src/app/controllers/languages/batches/__init__.py
+++ src/app/controllers/languages/batches/__init__.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template, url_for, redirect
 
 from .graphs import bp as graph_bp, GRAPH_DIR
+from ....forms.batch import BatchForm
+
 
 bp = Blueprint('batches', __name__, url_prefix='/batches')
 bp.register_blueprint(graph_bp)
@@ -11,6 +13,16 @@ def index(language):
     target_dir = GRAPH_DIR / language
     batches = [path for path in target_dir.iterdir() if path.is_dir()]
     return render_template("batches/index.j2", language=language, batches=batches)
+
+
+@bp.route("/new/")
+@bp.post("/new/")
+def new(language):
+    form = BatchForm(request.form)
+    form.language.process_data(language)
+    if request.method == "POST" and form.validate():
+        return redirect(url_for(".index", language=language))
+    return render_template("batches/new.j2", language=language, form=form)
 
 
 @bp.route("/<batch>/")
