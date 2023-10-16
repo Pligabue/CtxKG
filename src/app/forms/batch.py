8c3893d641531811ab5d84382c2cb662a8808775
Pedro Ligabue <pedroligabue@gmail.com>
2023-10-16 18:48:18 -0300

    Add the batch form
    This form will be used to trigger the generation of a new batch of
    graphs. The most relevant fields are the name of the batch (which may
    not repeat), the language, the BERT size and the text files that will
    serve as the base documents.

---
+++ src/app/forms/batch.py
@@ -0,0 +1,21 @@
+from wtforms import Form, StringField, SelectField, IntegerField, FloatField, MultipleFileField, HiddenField
+from wtforms.validators import InputRequired, AnyOf, NumberRange
+
+
+class BatchForm(Form):
+    name = StringField("Batch name", [InputRequired()])
+    language = HiddenField("Language", [AnyOf(["en", "pt-BR"])])
+    bert_size = SelectField("Bert size", choices=[("small", "Small"), ("medium", "Medium"), ("big", "Big")])
+    filenames = MultipleFileField("Text files", [InputRequired()])
+    embedding_ratio = FloatField("Embedding ratio", [NumberRange(0.0, 1.0)], default=1.0)
+    similarity_threshold = FloatField("Similarity coef. (synonyms)", [NumberRange(0.0, 1.0)], default=0.8)
+    bridge_threshold = FloatField("Similarity coef. (bridges)", [NumberRange(0.0, 1.0)], default=0.7)
+    processing_batch_size = IntegerField("Processing batch size", default=300)
+
+    def show_advanced_options(self):
+        return (
+            self.embedding_ratio.data != self.embedding_ratio.default
+            or self.similarity_threshold.data != self.similarity_threshold.default
+            or self.bridge_threshold.data != self.bridge_threshold.default
+            or self.processing_batch_size.data != self.processing_batch_size.default
+        )
