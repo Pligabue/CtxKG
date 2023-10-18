c4ecff36b4ce6edc2a3b9eea6dd383a529a981ec
Pedro Ligabue <pedroligabue@gmail.com>
2023-10-17 22:24:10 -0300

    Add alerts to the base template
    The secret is being generated randomly at every startup. This was done
    just that the flash element can be used. If this app ever becomes
    something that actually requires sessions, the secret should be
    generated once and then kept in the environment variables.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import secrets
 
 from .controllers import language_bp, home_bp
 
@@ -6,3 +7,4 @@ from .controllers import language_bp, home_bp
 app = Flask(__name__)
 app.register_blueprint(language_bp)
 app.register_blueprint(home_bp)
+app.secret_key = secrets.token_hex()
