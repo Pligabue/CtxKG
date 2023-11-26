af3562faac7758006e1eb09c1c5d5e8915a9002a
Pedro Ligabue <pedroligabue@gmail.com>
2023-11-26 13:06:30 -0300

    Run the startup tasks after configuring the server

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,9 +3,12 @@ import secrets
 
 from .controllers.batches import bp as batch_bp
 from .controllers.home import bp as home_bp
+from .tasks.startup import startup
 
 
 app = Flask(__name__)
 app.register_blueprint(batch_bp)
 app.register_blueprint(home_bp)
 app.secret_key = secrets.token_hex()
+
+startup()
